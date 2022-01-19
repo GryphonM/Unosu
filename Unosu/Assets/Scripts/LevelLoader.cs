cbcd30b41c2f4f6547d6dfdb2513d158251e3ba9
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2022-01-19 14:01:54 -0800

    Restat

--- Unosu/Assets/Scripts/LevelLoader.cs
+++ Unosu/Assets/Scripts/LevelLoader.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
+    public int CurrentLevel = 1;
     public GameObject[] levels;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ResetLevel(int level)
     {
-        GameObject oldLevel = FindObjectOfType<Level>().gameObject;
-        if (oldLevel != null)
-            Destroy(oldLevel);
+        //account for arrays starting at zero
+        level = level - 1;        
+        if (FindObjectOfType<Level>() != null)
+            Destroy(FindObjectOfType<Level>().gameObject);
         Instantiate(levels[level]);
     }
+    public void NextLevel()
+    {
+        CurrentLevel += 1;
+        ResetLevel(CurrentLevel);
+    }
 }
+
