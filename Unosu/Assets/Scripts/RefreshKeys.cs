a956bdbf903e62bf7f5041f73d82a89bde13286b
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2022-01-20 13:15:48 -0800

    Key refresh

--- Unosu/Assets/Scripts/RefreshKeys.cs
+++ Unosu/Assets/Scripts/RefreshKeys.cs
@@ -8,11 +8,12 @@ public class RefreshKeys : MonoBehaviour
     // 1 = jump
     // 2 = left
     // 3 = right
-    // 4 = slide?
+    // 4 = slide
+    PlayerController playerCont;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCont = FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
@@ -27,8 +28,25 @@ public class RefreshKeys : MonoBehaviour
         {
             switch(KeyToRefresh)
             {
+                case 0:
+                    Debug.Log("Key to refresh not asigned");
+                    Destroy(this.gameObject);
+                    break;
                 case 1:
-                    //write refresh code here
+                    playerCont.canJump = true;
+                    Destroy(this.gameObject);
+                    break;
+                case 2:
+                    playerCont.canMoveLeft = true;
+                    Destroy(this.gameObject);
+                    break;
+                case 3:
+                    playerCont.canMoveRight = true;
+                    Destroy(this.gameObject);
+                    break;
+                case 4:
+                    playerCont.canSlide = true;
+                    Destroy(this.gameObject);
                     break;
             }
         }
