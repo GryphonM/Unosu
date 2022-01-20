4358f1e6a25a0ae128ffa25967476d2fadbdb320
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2022-01-20 13:37:22 -0800

    BouncePad

---
+++ Unosu/Assets/Scripts/BouncePads.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePads : MonoBehaviour
+{
+    public float thrust = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.up * thrust);
+        }
+    }
+}
