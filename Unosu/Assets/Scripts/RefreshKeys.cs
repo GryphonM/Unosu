824f8a3cbe852e2631b02771187d1ef90de4c32f
Gryphon M <71658735+GryphonM@users.noreply.github.com>
2022-01-21 13:47:38 -0800

    Key Refresh Colors
    Accidentally made pacman colors

--- Unosu/Assets/Scripts/RefreshKeys.cs
+++ Unosu/Assets/Scripts/RefreshKeys.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class RefreshKeys : MonoBehaviour
 {
+    [Tooltip("1 = Jump\n" +
+            "2 = Left\n" +
+            "3 = Right\n" +
+            "4 = Slide")]
     public int KeyToRefresh = 0;
     // 1 = jump
     // 2 = left
     // 3 = right
     // 4 = slide
     PlayerController playerCont;
+    SpriteRenderer mySR;
+
+    [SerializeField] Color JumpColor;
+    [SerializeField] Color LeftColor;
+    [SerializeField] Color RightColor;
+    [SerializeField] Color SlideColor;
+
     // Start is called before the first frame update
     void Start()
     {
         playerCont = FindObjectOfType<PlayerController>();
+        mySR = GetComponent<SpriteRenderer>();
+        switch(KeyToRefresh)
+        {
+            case 0:
+                Debug.Log("Key to refresh not assigned");
+                break;
+            case 1:
+                mySR.color = JumpColor;
+                break;
+            case 2:
+                mySR.color = LeftColor;
+                break;
+            case 3:
+                mySR.color = RightColor;
+                break;
+            case 4:
+                mySR.color = SlideColor;
+                break;
+        }
     }
 
     // Update is called once per frame
